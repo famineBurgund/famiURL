626fb8d616397c4313243c75095b805e754f5fb7
fami <rosariumaphel@gmail.com>
2026-02-14 21:16:08 +1000

    logger init

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/famineBurgund/famiURL/internal/config"
+)
+
+const (
+	EnvLocal = "local"
+	EnvDev   = "dev"
+	EnvProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enable")
 	// TODO: init config: cleanenv
 
 	// TODO: init logger: log/slog
@@ -15,4 +29,25 @@ func main() {
 	// TODO: init router: chi, render
 
 	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case EnvLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case EnvDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case EnvProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
